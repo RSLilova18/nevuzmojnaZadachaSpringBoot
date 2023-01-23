77f96c557cf4a9fc3b43ce792a85302a1d544c9f
RSLilova18 <RSLilova18@codingburgas.bg>
2023-01-23 12:25:37 +0200

    Initial commit

---
+++ src/main/java/com/example/nevuzmojnazadachaspringboot/NevuzmojnaZadachaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.nevuzmojnazadachaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NevuzmojnaZadachaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NevuzmojnaZadachaSpringBootApplication.class, args);
+    }
+
+}
